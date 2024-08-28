fac27c07065edc1c69566684db449aafb4256d56
Deepak's PC <d33paksharma.39@gmail.com>
2024-08-28 15:23:31 +0530

    safari compatibility

--- src/component/footer/responsive/footer/topFooter/sections/LinkSection.js
+++ src/component/footer/responsive/footer/topFooter/sections/LinkSection.js
@@ -2,22 +2,26 @@ import React from 'react'
 import Style from './LinkSection.module.css'
 import LinkItem from './LinkItem'
 import FormSection from './FormSection';
+import { domain } from '../../../../../../domain';
 const LinkSection = () => {
   const information = [
-    { src: "/", name: "About Company" },
-    { src: "/", name: "Blogs" },
-    { src: "/", name: "Contact Us" },
-    { src: "/", name: "Team" },
-    { src: "/", name: "Carrier" },
+    { src: `${domain}/xperia-group`, name: "Xperia Group" },
+    { src: `${domain}/xperia-alive`, name: "Xperia Alive" },
+    { src: `${domain}/pencil-box`, name: "Pencil Box" },
+    { src: `${domain}/blogs`, name: "Blogs" },
+    { src: `${domain}/get-in-touch`, name: "Contact Us" },
+    { src:  `${domain}/team`, name: "Team" },
+    { src: `${domain}/team`, name: "Carrier" },
   ];
   const services = [
-    { src: "/", name: "Out of Home" },
-    { src: "/", name: "Multiplex Cinema Advertising" },
-    { src: "/", name: "Events & Promotions" },
-    { src: "/", name: "Content Design" },
-    { src: "/", name: "Creative retainership" },
-    { src: "/", name: "Corporate Films" },
-    { src: "/", name: "Music" },
+    { src: `${domain}/out-of-home`, name: "Out of Home" },
+    { src: `${domain}/multiplex`, name: "Multiplex " },
+    { src: `${domain}/cinema-advertising`, name: "Cinema Advertising" },
+    { src: `${domain}/events-and-promotions`, name: "Events & Promotions" },
+    { src: `${domain}/content-design`, name: "Content Design" },
+    { src: `${domain}/creative-retainership`, name: "Creative Retainership" },
+    { src: `${domain}/corporate-films`, name: "Corporate Films" },
+    { src: `${domain}/music`, name: "Music" },
   ];
   return (
     <>
