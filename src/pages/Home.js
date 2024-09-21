27d59a02d06785ba0eaaa877b4c5c06fb8a077a1
Deepak's PC <d33paksharma.39@gmail.com>
2024-09-21 11:32:49 +0530

    safari load config

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -33,19 +33,6 @@ const Home = () => {
     };
   }, [screenSize]);
 
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = '/assets/js/wp-content/mk-widgets.min.js';
-    script.id = 'mk-widgets-js';
-
-    // Append the script to the body
-    document.body.appendChild(script);
-
-    // Cleanup function to remove the script when the component unmounts
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []); // Empty dependency array to ensure the effect runs only once
 
   return (
     <>
