163bf781c7df20de868c7a3b5116c6c075ec050e
Deepak's PC <d33paksharma.39@gmail.com>
2024-09-21 15:29:57 +0530

    font config

--- src/index.js
+++ src/index.js
@@ -6,11 +6,9 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import PersistProvider from "./redux/providers/persist-provider";
 import { store } from "./redux/store";
-import '@babel/polyfill';
+import "@babel/polyfill";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-
 
 root.render(
   <React.StrictMode>
